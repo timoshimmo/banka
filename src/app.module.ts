20f9874b1a3b6c00b538bca69d0675f7b42bc98a
McToluene <xclusiveniggaz10@live.com>
2022-07-16 21:20:57 +0100

    Mongoose added and account module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    AccountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
