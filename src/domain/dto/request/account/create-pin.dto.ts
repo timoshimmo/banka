62599aeb7dbed8f63d95197b13dd8d188b0098dd
McToluene <xclusiveniggaz10@live.com>
2022-07-24 12:51:28 +0100

    Pin validation added

--- src/domain/dto/request/account/create-pin.dto.ts
+++ src/domain/dto/request/account/create-pin.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, Min } from 'class-validator';
+import { IsNumber, Max, Min } from 'class-validator';
 
 export default class CreatePinDto {
   @IsNumber()
-  @Min(6)
+  @Min(100000, { message: 'Pin must not be less than 6' })
+  @Max(999999, { message: 'Pin must not be more than 6' })
   @ApiProperty()
   pin: number;
 
